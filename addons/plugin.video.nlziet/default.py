e3018f466224c2da72877cff96e679e9b36860a7
Didier Dirks <didier.dirks@chep.com>
2026-06-15 16:54:16 +0200

    debugging content

--- addons/plugin.video.nlziet/default.py
+++ addons/plugin.video.nlziet/default.py
@@ -2577,10 +2577,13 @@ def play_item(content_id, fmt=None, st=None):
                 # date = datetime.fromtimestamp(int(st), tz=timezone.utc).strftime('%Y-%m-%d')
 
                 # add 1 minute to st to ensure we get the current program if st is exactly at the start of a program
-                st_plus_1min = int(st) + 60
+                st_plus_1min = int(st) + 300
                 epg_map = api.get_current_programs(channel_ids=[content_id], start_time=st_plus_1min)
-                xbmcgui.Dialog().ok('NLZiet', f'Treating as live TV\nfmt={fmt}\ncontent_id={content_id}\nst={st}')
-                xbmcgui.Dialog().ok('NLZiet', f'{epg_map}')
+
+                content = epg_map.get(content_id).get("current").get("raw").get("content")
+
+                # xbmcgui.Dialog().ok('NLZiet', f'Treating as live TV\nfmt={fmt}\ncontent_id={content_id}\nst={st}')
+                xbmcgui.Dialog().ok('NLZiet', f'{content}')
             except Exception as e:
                 xbmc.log(f"NLZiet get_current_programs failed: {e}", xbmc.LOGWARNING)
 
